d08d87dbdffea089365b410135daa6c1db1f41a1
Leonardo <leojportes@gmail.com>
2022-06-11 13:48:12 -0300

    teste

--- pages/api/recommendation.js
+++ pages/api/recommendation.js
@@ -1,84 +1,94 @@
 
 // Recommendations endpoint
+// async function recommendation(request, response) {
+
+//     response.json({
+//         recommendations: [{
+//             movies: [{
+//                 title: "O lobo de Wall Street",
+//                 description: "Descrição do filme",
+//                 release: "2014",
+//                 duration: "1h30min"
+//             },
+//             {
+//                 title: "Filme 2",
+//                 description: "Descrição do filme",
+//                 release: "2014",
+//                 duration: "1h30min"
+//             },
+//             {
+//                 title: "Filme 3",
+//                 description: "Descrição do filme",
+//                 release: "2014",
+//                 duration: "1h30min"
+//             },
+//             {
+//                 title: "Filme 4",
+//                 description: "Descrição do filme",
+//                 release: "2014",
+//                 duration: "1h30min"
+//             }],
+//             tvshow: [{
+//                 title: "Série 1",
+//                 description: "Descrição do Série",
+//                 release: "2014",
+//                 duration: "1h30min"
+//             },
+//             {
+//                 title: "Série 2",
+//                 description: "Descrição do Série",
+//                 release: "2014",
+//                 duration: "1h30min"
+//             },
+//             {
+//                 title: "Série 3",
+//                 description: "Descrição do Série",
+//                 release: "2014",
+//                 duration: "1h30min"
+//             },
+//             {
+//                 title: "Série 4",
+//                 description: "Descrição do Série",
+//                 release: "2022",
+//                 duration: "1h30min"
+//             }],
+//             books: [{
+//                 title: "Livro 1",
+//                 description: "Descrição do Livro",
+//                 release: "2017",
+//                 pages: "1h30min"
+//             },
+//             {
+//                 title: "Livro 2",
+//                 description: "Descrição do Livro",
+//                 release: "2014",
+//                 pages: "1h30min"
+//             },
+//             {
+//                 title: "Livro 3",
+//                 description: "Descrição do Livro",
+//                 release: "2011",
+//                 pages: "400 pages"
+//             },
+//             {
+//                 title: "Livro 4",
+//                 description: "Descrição do Livro",
+//                 release: "2010",
+//                 pages: "345 pages"
+//             }],
+//         }]
+//     });
+
+// }
+
 async function recommendation(request, response) {
 
     response.json({
-        recommendations: [{
-            movies: [{
-                title: "O lobo de Wall Street",
-                description: "Descrição do filme",
-                release: "2014",
-                duration: "1h30min"
-            },
-            {
-                title: "Filme 2",
-                description: "Descrição do filme",
-                release: "2014",
-                duration: "1h30min"
-            },
-            {
-                title: "Filme 3",
-                description: "Descrição do filme",
-                release: "2014",
-                duration: "1h30min"
-            },
-            {
-                title: "Filme 4",
-                description: "Descrição do filme",
-                release: "2014",
-                duration: "1h30min"
-            }],
-            tvshow: [{
-                title: "Série 1",
-                description: "Descrição do Série",
-                release: "2014",
-                duration: "1h30min"
-            },
-            {
-                title: "Série 2",
-                description: "Descrição do Série",
-                release: "2014",
-                duration: "1h30min"
-            },
-            {
-                title: "Série 3",
-                description: "Descrição do Série",
-                release: "2014",
-                duration: "1h30min"
-            },
-            {
-                title: "Série 4",
-                description: "Descrição do Série",
-                release: "2022",
-                duration: "1h30min"
-            }],
-            books: [{
-                title: "Livro 1",
-                description: "Descrição do Livro",
-                release: "2017",
-                pages: "1h30min"
-            },
-            {
-                title: "Livro 2",
-                description: "Descrição do Livro",
-                release: "2014",
-                pages: "1h30min"
-            },
-            {
-                title: "Livro 3",
-                description: "Descrição do Livro",
-                release: "2011",
-                pages: "400 pages"
-            },
-            {
-                title: "Livro 4",
-                description: "Descrição do Livro",
-                release: "2010",
-                pages: "345 pages"
-            }],
-        }]
-    });
+        movie: "TESTE"
+    })
+  
 
 }
+
 
 export default recommendation;
