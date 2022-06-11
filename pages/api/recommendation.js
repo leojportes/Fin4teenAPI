93bf43db7189ce926e1cd459fa5c84b86edb7b4f
Leonardo <leojportes@gmail.com>
2022-06-11 01:54:33 -0300

    create movie list example

--- pages/api/recommendation.js
+++ pages/api/recommendation.js
@@ -1,9 +1,30 @@
 async function recommendation(request, response) {
     response.json({
-        title: "O lobo de Wall Street",
-        description: "Descrição do filme",
-        release: "2014",
-        duration: "1h30min"
+        recommendations: [{
+            title: "O lobo de Wall Street",
+            description: "Descrição do filme",
+            release: "2014",
+            duration: "1h30min"
+        },
+        {
+            title: "Filme 2",
+            description: "Descrição do filme",
+            release: "2014",
+            duration: "1h30min"
+        },
+        {
+            title: "Filme 3",
+            description: "Descrição do filme",
+            release: "2014",
+            duration: "1h30min"
+        },
+        {
+            title: "Filme 4",
+            description: "Descrição do filme",
+            release: "2014",
+            duration: "1h30min"
+        }
+        ]
     });
 }
 
